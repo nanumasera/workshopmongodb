64c32029578389337980522f5bd8dd3683c0a6f3
Nanu <nanumasera@hotmail.com>
2018-12-14 16:21:35 -0200

    delete user

--- src/main/java/com/nanuworkshopmongo/services/UserService.java
+++ src/main/java/com/nanuworkshopmongo/services/UserService.java
@@ -16,19 +16,26 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User>findAll(){
-		return repo.findAll();		
+
+	public List<User> findAll() {
+		return repo.findAll();
 	}
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}	
-	public User insert (User obj) {
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
-	public User fromDTO (UserDTO objDto) {
-		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
