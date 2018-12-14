864b8e27d9f9a12122e827190d45b74dc3f7f755
Nanu <nanumasera@hotmail.com>
2018-12-14 19:31:24 -0200

    Referenciando posts do user

--- src/main/java/com/nanuworkshopmongo/domain/User.java
+++ src/main/java/com/nanuworkshopmongo/domain/User.java
@@ -1,18 +1,24 @@
 package com.nanuworkshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
-public class User implements Serializable{	
+@Document(collection = "user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy=true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -46,6 +52,15 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
@@ -72,4 +87,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
 }
